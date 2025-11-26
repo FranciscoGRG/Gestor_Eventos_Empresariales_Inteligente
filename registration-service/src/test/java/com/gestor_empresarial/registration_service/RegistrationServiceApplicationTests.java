16e357869f0ec36aaaaab5effaa790aeca6774c5
Fran <fgrcalifa@gmail.com>
2025-11-26 12:23:24 +0100

    primer comit

---
+++ registration-service/src/test/java/com/gestor_empresarial/registration_service/RegistrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestor_empresarial.registration_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
