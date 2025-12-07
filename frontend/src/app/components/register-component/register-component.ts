e933571a9a8727eaa91c1e9e98ebc5f760855762
Fran <fgrcalifa@gmail.com>
2025-12-07 15:54:32 +0100

    completado el componente register

--- frontend/src/app/components/register-component/register-component.ts
+++ frontend/src/app/components/register-component/register-component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth-service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register-component',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule, CommonModule],
   templateUrl: './register-component.html',
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
 
+  registerForm!: FormGroup;
+  errorMessage: string | null = null;
+
+  private fb = inject(FormBuilder);
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+    })
+  }
+
+  onSubmit(): void {
+    this.errorMessage = null;
+
+    if (this.registerForm.valid) {
+      this.authService.register(this.registerForm.value).subscribe({
+        next: () => { this.router.navigate(['/'])},
+        error: (error) => {
+          this.errorMessage = "Error al registrar al usuario"
+          console.error("Error al registrar al usuario: ", error)
+        }
+      })
+    }
+  }
 }
